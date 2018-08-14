ef5adaa4210bb853195e5e8e1e0c45f2b17cf979
Sho Yashiro <shirotani@seginus.jp>
2018-08-15 01:56:18 +0900

    phpミス修正

--- form.php
+++ form.php
@@ -4,7 +4,7 @@ mb_internal_encoding('UTF-8');
 
 $tel = mb_convert_kana($_REQUEST['Tel'],'n','UTF-8');
 
-$success=mb_send_mail('shirotani@seginus.jp',$_POST['Question'],$_POST['Naiyou'].$_POST['Name'].$_POST['Tel'],$_POST['Mail']);
+$success=mb_send_mail('shirotani@seginus.jp',$_POST['Question'],$_POST['Naiyou'].$_POST['Name'].$tel,$_POST['Mail']);
 
 if($success)echo("お問い合わせを送信しました。お返事までお時間をいただくことがございますが、何とぞご了承ください。");
 ?>
